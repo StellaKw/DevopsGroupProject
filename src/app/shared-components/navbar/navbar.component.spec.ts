f5abab7c5fc257db958b977befdb662abcddc649
Yusuf Adeleke <yadeleke@my.centennialcollege.ca>
2025-04-12 21:46:02 -0400

    fix test cases

--- src/app/shared-components/navbar/navbar.component.spec.ts
+++ src/app/shared-components/navbar/navbar.component.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { ActivatedRoute } from "@angular/router";
+import { of } from "rxjs";
+import { RouterTestingModule } from "@angular/router/testing";
 
 import { NavbarComponent } from "./navbar.component";
 
@@ -8,7 +11,19 @@ describe("NavbarComponent", () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NavbarComponent],
+      imports: [
+        NavbarComponent, // standalone component
+        RouterTestingModule, // satisfies routerLink, routerOutlet, etc.
+      ],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {},
+            params: of({}), // mock observable if you use route params
+          },
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(NavbarComponent);
